d6f361aa0764ac2619921e1ebd833dec6257a2e8
NoorRahman1234 <161183531+NoorRahman1234@users.noreply.github.com>
2026-04-06 11:44:04 +0500

    Add the mongodb Atlas

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/test");
+mongoose.connect("mongodb+srv://rahmanafr0123_db_user:rfvgbH5BRcoRxkci@cluster0.s9u5g2n.mongodb.net/?appName=Cluster0");
 
 app.get("/get", (req, res) => {
   TodoModel.find()
